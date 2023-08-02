fb9597340096e27357d76638da525bc7f9398c58
ashtray4715 <gobinda.paul.4715@gmail.com>
2023-08-03 02:37:37 +0600

    Adding test cl

--- app/src/main/java/com/ashtray/admob/ads/tester/MainActivity.kt
+++ app/src/main/java/com/ashtray/admob/ads/tester/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ashtray.admob.ads.tester
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: test")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
